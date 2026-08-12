5928e24e634ff3dd52d1b5090924194bf4cdb84e
Utkarshnegi2k5 <utkarshnegi05@gmail.com>
2026-08-12 15:45:16 +0530

    testing

--- fern/custom.js
+++ fern/custom.js
@@ -1,9 +1,12 @@
-import docsearch from '@docsearch/js';
-import '@docsearch/css';
+const searchClient = algoliasearch(
+  "PV6ET6Q7MW",
+  "d2ac55797e41cc69b25486582a39c4a5"
+);
 
-docsearch({
-  container: '#docsearch',
-  appId: 'PV6ET6Q7MW',
-  indexName: 'monocloud_docs',
-  apiKey: 'd2ac55797e41cc69b25486582a39c4a5'
-});
+const index = searchClient.initIndex("monocloud_docs");
+
+async function searchMonocloud(query) {
+  const { hits } = await index.search(query);
+
+  console.log(hits);
+}

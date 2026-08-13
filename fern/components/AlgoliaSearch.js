16c8ac83803e4fe76029821edccad7fa9185990e
Utkarshnegi2k5 <utkarshnegi05@gmail.com>
2026-08-13 11:34:32 +0530

    testing

---
+++ fern/components/AlgoliaSearch.js
@@ -0,0 +1,9 @@
+import docsearch from '@docsearch/js';
+
+
+docsearch({
+  container: '#docsearch',
+  appId: 'PV6ET6Q7MW',
+  indexName: 'monocloud_docs',
+  apiKey: 'd2ac55797e41cc69b25486582a39c4a5',
+});
